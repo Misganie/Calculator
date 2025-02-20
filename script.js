290a16cb21a8828999272e6c6660184c2d89580e
amare <amare2020@gmail.com>
2025-02-20 21:57:17 +0300

    Creating functions for basic math operations

---
+++ script.js
@@ -0,0 +1,20 @@
+// Functions for basic math operators
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Error: Can't divide by 0";
+    }
+    return a / b;
+}
