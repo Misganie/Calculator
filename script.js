21923e06b3ce75cc8859c703d570c42760e9a496
amare <amare2020@gmail.com>
2025-02-20 22:17:59 +0300

    Implementing the operate Function

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ function divide(a, b) {
     }
     return a / b;
 }
+
+// Creating a new function  that takes an operator and two numbers
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return null;
+    }
+  }
+  
